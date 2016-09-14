ba07fa201c5ee3032007f7f2d2daa83d5d163576
Nicholas Nadeau <nicholas.nadeau.1@ens.etsmtl.ca>
2016-09-13 20:49:30 -0400

    added javadoc

--- src/main/java/me/nicholasnadeau/robotiq/ft/RobotiqFt.java
+++ src/main/java/me/nicholasnadeau/robotiq/ft/RobotiqFt.java
@@ -55,6 +55,11 @@ public class RobotiqFt {
         this.modbusSerialMaster = new ModbusSerialMaster(this.getSerialParameters());
     }
 
+    /**
+     * Convenient connection test
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Logger.info("Running simple RobotiqFT connection test");
         RobotiqFt robotiqFt = new RobotiqFt();
@@ -76,110 +81,191 @@ public class RobotiqFt {
         }
     }
 
+    /**
+     * @return unit ID of sensor
+     */
     public int getUnitID() {
         return this.unitID;
     }
 
+    /**
+     * @param unitID of sensor
+     */
     public void setUnitID(int unitID) {
         this.unitID = unitID;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getFxRegister() {
         return this.fxRegister;
     }
 
+    /**
+     * @param fxRegister modbus register
+     */
     public void setFxRegister(int fxRegister) {
         this.fxRegister = fxRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getFyRegister() {
         return this.fyRegister;
     }
 
+    /**
+     * @param fyRegister modbus register
+     */
     public void setFyRegister(int fyRegister) {
         this.fyRegister = fyRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getFzRegister() {
         return this.fzRegister;
     }
 
+    /**
+     * @param fzRegister modbus register
+     */
     public void setFzRegister(int fzRegister) {
         this.fzRegister = fzRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getMxRegister() {
         return this.mxRegister;
     }
 
+    /**
+     * @param mxRegister modbus register
+     */
     public void setMxRegister(int mxRegister) {
         this.mxRegister = mxRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getMyRegister() {
         return this.myRegister;
     }
 
+    /**
+     * @param myRegister modbus register
+     */
     public void setMyRegister(int myRegister) {
         this.myRegister = myRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getMzRegister() {
         return this.mzRegister;
     }
 
+    /**
+     * @param mzRegister modbus register
+     */
     public void setMzRegister(int mzRegister) {
         this.mzRegister = mzRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getAxRegister() {
         return this.axRegister;
     }
 
+    /**
+     * @param axRegister modbus register
+     */
     public void setAxRegister(int axRegister) {
         this.axRegister = axRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getAyRegister() {
         return this.ayRegister;
     }
 
+    /**
+     * @param ayRegister modbus register
+     */
     public void setAyRegister(int ayRegister) {
         this.ayRegister = ayRegister;
     }
 
+    /**
+     * @return modbus register
+     */
     public int getAzRegister() {
         return this.azRegister;
     }
 
+    /**
+     * @param azRegister modbus register
+     */
     public void setAzRegister(int azRegister) {
         this.azRegister = azRegister;
     }
 
+    /**
+     * @return modbus value divisor
+     */
     public double getForceDivisor() {
         return this.forceDivisor;
     }
 
+    /**
+     * @param forceDivisor modbus value divisor
+     */
     public void setForceDivisor(double forceDivisor) {
         this.forceDivisor = forceDivisor;
     }
 
+    /**
+     * @return modbus value divisor
+     */
     public double getMomentDivisor() {
         return this.momentDivisor;
     }
 
+    /**
+     * @param momentDivisor modbus value divisor
+     */
     public void setMomentDivisor(double momentDivisor) {
         this.momentDivisor = momentDivisor;
     }
 
+    /**
+     * @return modbus value divisor
+     */
     public double getAccelerationDivisor() {
         return this.accelerationDivisor;
     }
 
+    /**
+     * @param accelerationDivisor modbus value divisor
+     */
     public void setAccelerationDivisor(double accelerationDivisor) {
         this.accelerationDivisor = accelerationDivisor;
     }
 
+    /**
+     * @return success of loading process
+     */
     private boolean loadProperties() {
         Properties properties = new Properties();
         boolean isSuccess = true;
@@ -231,60 +317,100 @@ public class RobotiqFt {
         return isSuccess;
     }
 
+    /**
+     * @return measured force [N]
+     * @throws ModbusException
+     */
     public double getFx() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getFxRegister(), 1)[0];
         return (register.toShort() / this.getForceDivisor());
     }
 
+    /**
+     * @return measured force [N]
+     * @throws ModbusException
+     */
     public double getFy() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getFyRegister(), 1)[0];
         return (register.toShort() / this.getForceDivisor());
     }
 
+    /**
+     * @return measured force [N]
+     * @throws ModbusException
+     */
     public double getFz() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getFzRegister(), 1)[0];
         return (register.toShort() / this.getForceDivisor());
     }
 
+    /**
+     * @return measured moment [Nm]
+     * @throws ModbusException
+     */
     public double getMx() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getMxRegister(), 1)[0];
         return (register.toShort() / this.getMomentDivisor());
     }
 
+    /**
+     * @return measured moment [Nm]
+     * @throws ModbusException
+     */
     public double getMy() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getMyRegister(), 1)[0];
         return (register.toShort() / this.getMomentDivisor());
     }
 
+    /**
+     * @return
+     * @throws ModbusException
+     */
     public double getMz() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getMzRegister(), 1)[0];
         return (register.toShort() / this.getMomentDivisor());
     }
 
+    /**
+     * @return measured acceleration [g]
+     * @throws ModbusException
+     */
     public double getAx() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getAxRegister(), 1)[0];
         return (register.toShort() / this.getAccelerationDivisor());
     }
 
+    /**
+     * @return measured acceleration [g]
+     * @throws ModbusException
+     */
     public double getAy() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getAyRegister(), 1)[0];
         return (register.toShort() / this.getAccelerationDivisor());
     }
 
+    /**
+     * @return measured acceleration [g]
+     * @throws ModbusException
+     */
     public double getAz() throws ModbusException {
         InputRegister register = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getAzRegister(), 1)[0];
         return (register.toShort() / this.getAccelerationDivisor());
     }
 
+    /**
+     * @return measured wrench {Fx, Fy, Fz, Mx, My, Mz} [N, N, N, Nm, Nm, Nm]
+     * @throws ModbusException
+     */
     public double[] getSixAxisMeasure() throws ModbusException {
         InputRegister[] registers = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getFxRegister(), 6);
@@ -304,6 +430,10 @@ public class RobotiqFt {
         return result;
     }
 
+    /**
+     * @return measured wrench and accelerations {Fx, Fy, Fz, Mx, My, Mz, Ax, Ay, Az} [N, N, N, Nm, Nm, Nm, g, g, g]
+     * @throws ModbusException
+     */
     public double[] getCompleteMeasure() throws ModbusException {
         InputRegister[] wrenchRegisters = this.getModbusSerialMaster()
                 .readInputRegisters(this.getUnitID(), this.getFxRegister(), 6);
@@ -328,37 +458,64 @@ public class RobotiqFt {
         return result;
     }
 
+    /**
+     * @return measured forces {Fx, Fy, Fz} [N, N, N]
+     * @throws ModbusException
+     */
     public double[] getForces() throws ModbusException {
         double[] data = {this.getFx(), this.getFy(), this.getFz()};
         return data;
     }
 
+    /**
+     * @return measured moments {Mx, My, Mz} [Nm, Nm, Nm]
+     * @throws ModbusException
+     */
     public double[] getMoments() throws ModbusException {
         double[] data = {this.getMx(), this.getMy(), this.getMz()};
         return data;
     }
 
+    /**
+     * @return measured accelerations {Ax, Ay, Az} [g, g, g]
+     * @throws ModbusException
+     */
     public double[] getAccelerations() throws ModbusException {
         double[] data = {this.getAx(), this.getAy(), this.getAz()};
         return data;
     }
 
+    /**
+     * @return modbus client object
+     */
     public ModbusSerialMaster getModbusSerialMaster() {
         return this.modbusSerialMaster;
     }
 
+    /**
+     * @param modbusSerialMaster modbus client object
+     */
     public void setModbusSerialMaster(ModbusSerialMaster modbusSerialMaster) {
         this.modbusSerialMaster = modbusSerialMaster;
     }
 
+    /**
+     * @throws Exception if connection to sensor fails
+     */
     private void connect() throws Exception {
         this.getModbusSerialMaster().connect();
     }
 
+    /**
+     * @return serial parameters
+     */
     public SerialParameters getSerialParameters() {
         return this.serialParameters;
     }
 
+    /**
+     * @param serialParameters
+     */
     public void setSerialParameters(SerialParameters serialParameters) {
         this.serialParameters = serialParameters;
     }
